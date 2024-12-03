601dc9c16c854525924cb18855e66f5566f2fb04
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-12-03 15:07:16 +0900

    ok

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-var add = function (n1, n2) {
-    return n1 + n2;
+var add = function (n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return n1 + n2;
+    }
 };
 var num1 = 5;
 var num2 = 2.5;
-var result = add(num1, num2);
-console.log(result);
+var printResult = true;
+var resultPhrase = 'Result is ....';
+add(num1, num2, printResult, resultPhrase);
--- app.ts
+++ app.ts
@@ -1,10 +1,19 @@
-const add = (n1: number,n2: number) => {
-    return n1 + n2;
+const add = (n1: number,n2: number,showResult: boolean,phrase: string) => {
+    const result = n1 + n2;
+
+    if(showResult){
+        console.log(phrase + result);
+        
+    }else{
+        return n1 + n2
+    }
 }
+
 
 const num1 = 5;
 const num2 = 2.5;
+const printResult = true;
+const resultPhrase = 'Result is ....'
 
-const result = add(num1,num2)
-console.log(result);
+add(num1,num2,printResult,resultPhrase);
 
